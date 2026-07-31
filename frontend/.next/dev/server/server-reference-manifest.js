3186c01440289b4b01e3e4cbc29ac655dcb6ba47
Vinicius Prado <viniciusprado99@gmail.com>
2026-07-31 21:58:02 +0000

    SQLite -> PostgreSQL

--- frontend/.next/dev/server/server-reference-manifest.js
+++ frontend/.next/dev/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jxyT/o+QwfQFe6/a/I+G8J1wJ1R+vebGdbuFgz7Xo3M=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"dsJ7J+4IoK0hur1cq2r6ItMcjtCod57++I6eWpt3V9s=\"\n}"
